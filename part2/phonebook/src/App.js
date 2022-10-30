049454f19e7aa00fdcede3e53af70ae1f2ab6cdb
name <>
2022-10-30 15:18:01 +0200

    Part 2.6 start - phonebook application

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,40 @@
+import { useState } from 'react'
+
+const App = () => {
+        const [persons, setPersons] = useState([
+                { name: 'Arto Hellas' }
+        ]) 
+        const [newName, setNewName] = useState('')
+
+        const addPerson = (event) => {
+                // prevent form submission / page reload
+                event.preventDefault()
+                const newPerson = {name: newName}
+                setPersons(persons.concat(newPerson))
+                setNewName("")
+        }
+        const handleNameFormChange = (event) => {
+                setNewName(event.target.value)
+        }
+
+
+        return (
+                <div>
+                <h2>Phonebook</h2>
+                <form onSubmit={addPerson}>
+                <div>
+                name: <input value={newName} onChange={handleNameFormChange}/>
+                </div>
+                <div>
+                <button type="submit">add</button>
+                </div>
+                </form>
+                <h2>Numbers</h2>
+                <ul>
+                {persons.map(person => <li key={person.name}>{person.name}</li>)}
+                </ul>
+                </div>
+        )
+}
+
+export default App
