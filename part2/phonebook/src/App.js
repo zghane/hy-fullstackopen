bc7db0e745936a19506694a499996080e1c028a1
name <>
2022-10-30 19:05:55 +0200

    Part 2.11 - fetch initial phonebook data from server

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,11 @@
-import { useState } from 'react'
+import { useState, useEffect} from 'react'
+import axios from "axios"
 import Filter from "./components/Filter"
 import PersonForm from "./components/PersonForm"
 import Persons from "./components/Persons"
 
 const App = () => {
-        const [persons, setPersons] = useState([
-                { name: 'Arto Hellas', number: '040-123456', id: 1 },
-                { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-                { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-                { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-        ]) 
+        const [persons, setPersons] = useState([]) 
         const [newName, setNewName] = useState("")
         const [newNumber, setNewNumber] = useState("")
         const [filterString, setFilterString] = useState("")
@@ -41,6 +37,17 @@ const App = () => {
                 return persons.filter(person =>
                         person.name.toLowerCase().includes(filterString.toLowerCase()))
         }
+
+        useEffect(() => {
+                console.log("effect")
+                axios
+                        .get("http://localhost:3001/persons")
+                        .then(response => {
+                                console.log("promise fulfill")
+                                setPersons(response.data)
+                        })
+        }, [])
+                        
 
         return (
                 <div>
--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import axios from "axios"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+axios.get("http://localhost:3001/persons").then(response => {
+        const persons = response.data
+        ReactDOM.createRoot(document.getElementById('root')).render(<App persons={persons} />)
+})
