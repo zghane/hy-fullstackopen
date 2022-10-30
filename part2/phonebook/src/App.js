d2fe58d4b93491b1e9669910fb7a6fbee1951762
name <>
2022-10-30 15:30:52 +0200

    Part 2.8 - store number for each person

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -2,18 +2,20 @@ import { useState } from 'react'
 
 const App = () => {
         const [persons, setPersons] = useState([
-                { name: 'Arto Hellas' }
+                { name: "Arto Hellas", number: "012345689" }
         ]) 
         const [newName, setNewName] = useState('')
+        const [newNumber, setNewNumber] = useState('')
 
         const addPerson = (event) => {
                 // prevent form submission / page reload
                 event.preventDefault()
                 // is the name new? (allow each person only once)
                 if (!persons.map(person => person.name).includes(newName)) {
-                const newPerson = {name: newName}
+                const newPerson = {name: newName, number: newNumber}
                         setPersons(persons.concat(newPerson))
                         setNewName("")
+                        setNewNumber("")
                 }
                 else {
                         alert(`${newName} is already added to the phonebook`)
@@ -22,22 +24,28 @@ const App = () => {
         const handleNameFormChange = (event) => {
                 setNewName(event.target.value)
         }
+        const handleNumberFormChange = (event) => {
+                setNewNumber(event.target.value)
+        }
 
 
         return (
                 <div>
                 <h2>Phonebook</h2>
                 <form onSubmit={addPerson}>
-                <div>
-                name: <input value={newName} onChange={handleNameFormChange}/>
-                </div>
-                <div>
-                <button type="submit">add</button>
-                </div>
+                        <div>
+                                name: <input value={newName} onChange={handleNameFormChange}/>
+                        </div>
+                        <div>
+                                number: <input value={newNumber} onChange={handleNumberFormChange}/>
+                        </div>
+                        <div>
+                                <button type="submit">add</button>
+                        </div>
                 </form>
                 <h2>Numbers</h2>
                 <ul>
-                {persons.map(person => <li key={person.name}>{person.name}</li>)}
+                {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
                 </ul>
                 </div>
         )
