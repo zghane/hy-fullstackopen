af7c1aebfcb8d1de9e5755f4cc9a8340389aa62f
name <>
2022-10-31 14:17:33 +0200

    Part 1.14 - show the most voted anecdote

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -11,11 +11,19 @@ const App = () => {
                 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
         ]
         const [selected, setSelected] = useState(0)
-        const [points, setPoints] = useState({})
+        const [points, setPoints] = useState(Array(anecdotes.length).fill(0))
 
         const Button = ({onClick, text}) => {
                 return (
                         <button onClick={onClick}>{text}</button>
+                )
+        }
+        const Anecdote = ({text, numVotes}) => {
+                return (
+                        <>
+                        <p>{text}</p>
+                        <p>has {numVotes} votes</p>
+                        </>
                 )
         }
         // select random index of array
@@ -27,26 +35,36 @@ const App = () => {
         }
         // increment votes for the currently selected/shown anecdote
         const incrementPoints = () => {
-            const newPoints = {...points}
-            if (newPoints[selected]) {
-                    newPoints[selected] += 1
-            }
-            else {
-                    newPoints[selected] = 1
-            }
+            const newPoints = Array.from(points)
+            newPoints[selected] += 1
             setPoints(newPoints)
-            console.log(points)
                 
+        }
+        // get the anecdote with most points/votes
+        // TODO: a more efficient solution
+        // now causes a lookup every time the state changes
+        const getMostVoted = () => {
+                var highestVotes = 0
+                var highestIndex = 0
+                for (var i=0; i < points.length; i++) {
+                        if (points[i] > highestVotes) {
+                                highestVotes = points[i]
+                                highestIndex = i
+                        }
+                }
+                return highestIndex
         }
 
 
 
         return (
                 <div>
-                <p>{anecdotes[selected]}</p>
-                <p>has {points[selected] ? points[selected] : 0} votes</p>
+                <h2>Anecdote of the day</h2>
+                <Anecdote text={anecdotes[selected]} numVotes={points[selected] ? points[selected] : 0} />
                 <Button onClick={chooseAnecdote} text="next anecdote" />
                 <Button onClick={incrementPoints} text="vote" />
+                <h2>Anecdote with most votes</h2>
+                <Anecdote text={anecdotes[getMostVoted()]} numVotes={points[getMostVoted()]} />
                 </div>
         )
 }
