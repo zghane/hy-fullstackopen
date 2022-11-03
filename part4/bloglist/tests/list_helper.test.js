7ab7956d1cc7809cd7df1a633234aeaf2d4b2b85
name <>
2022-11-03 17:38:55 +0000

    Part 4.5 - favoriteBlog function and corresponding tests

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -9,6 +9,7 @@ test("dummy returns one", () => {
 })
 
 // describe groups tests together
+// tests for totalLikes
 describe("total likes", () => {
         test("of empty list is zero", () => {
                 const blogs = []
@@ -51,7 +52,6 @@ describe("total likes", () => {
 
         // redundant
         test("of a bigger list is calculated right", () => {
-
                 const blogs = [
                           new Blog({
                                       _id: "5a422a851b54a676234d17f7",
@@ -107,4 +107,80 @@ describe("total likes", () => {
                 expect(result).toBe(36)
         })
 
+})
+
+// tests for formatBlog()
+test("format blog", () => {
+        const blog = new Blog({
+                _id: "5a422bc61b54a676234d17fc",
+                title: "Type wars",
+                author: "Robert C. Martin",
+                url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+                likes: 2,
+                __v: 0
+        })  
+        
+        const result = listHelper.formatBlog(blog)
+        expect(result).toEqual({
+                title: "Type wars",
+                author: "Robert C. Martin",
+                likes: 2
+        })
+})
+
+// tests for favoriteBlog()
+describe("favorite blog", () => {
+        test("of empty list", () => {
+                const blogs = []
+                const result = listHelper.favoriteBlog()
+                expect(result).toBe(null)
+        })
+
+        test("of a list of length one", () => {
+                const blogs = [
+                        new Blog({
+                                _id: "5a422a851b54a676234d17f7",
+                                title: "React patterns",
+                                author: "Michael Chan",
+                                url: "https://reactpatterns.com/",
+                                likes: 7,
+                                __v: 0
+                        }),
+                ]
+
+                const result = listHelper.favoriteBlog(blogs)
+                expect(result).toEqual(listHelper.formatBlog(blogs[0]))
+        })
+
+        test("of a list of multiple blogs", () => {
+                const blogs = [
+                          new Blog({
+                                      _id: "5a422a851b54a676234d17f7",
+                                      title: "React patterns",
+                                      author: "Michael Chan",
+                                      url: "https://reactpatterns.com/",
+                                      likes: 7,
+                                      __v: 0
+                                    }),
+                          new Blog({
+                                      _id: "5a422aa71b54a676234d17f8",
+                                      title: "Go To Statement Considered Harmful",
+                                      author: "Edsger W. Dijkstra",
+                                      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+                                      likes: 5,
+                                      __v: 0
+                                    }),
+                          new Blog({
+                                      _id: "5a422b3a1b54a676234d17f9",
+                                      title: "Canonical string reduction",
+                                      author: "Edsger W. Dijkstra",
+                                      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+                                      likes: 12,
+                                      __v: 0
+                                    }),
+                ]
+
+                const result = listHelper.favoriteBlog(blogs)
+                expect(result).toEqual(listHelper.formatBlog(blogs[2]))
+        })
 })
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,7 +2,7 @@ const dummy = (blogs) => {
         return 1
 }
 
-// count total number of likes for list of blogs
+// count total number of likes, given a list of blogs
 const totalLikes = (blogs) => {
         if (blogs && blogs.length > 0) {
                 return blogs.map(blog => blog.likes).reduce((sum, item) => {
@@ -14,7 +14,35 @@ const totalLikes = (blogs) => {
         }
 }
 
+// format a blog to show only relevant values
+const formatBlog = (blog) => {
+        return {
+                title: blog.title,
+                author: blog.author,
+                likes: blog.likes
+        }
+}
+
+// return the blog with most likes, given a list of blogs
+const favoriteBlog = (blogs) => {
+        if (blogs && blogs.length > 0) {
+                var maxLikes = blogs[0].likes
+                var maxLikesBlog = blogs[0]
+                for (var i = 0; i < blogs.length; i++) {
+                        if (blogs[i].likes > maxLikes) { 
+                            maxLikes = blogs[i].likes
+                            maxLikesBlog = blogs[i]
+                        }
+                }
+                return formatBlog(maxLikesBlog)
+        }
+        else {
+                return null
+        }
+}
 module.exports = {
         dummy,
-        totalLikes
+        totalLikes,
+        favoriteBlog,
+        formatBlog
 }
