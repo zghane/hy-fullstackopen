666b027700de6b76845a082829902de677642494
name <>
2022-11-03 15:42:08 +0000

    Part 4.2 - add middleware module

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -3,6 +3,7 @@ const app = express()
 const cors = require("cors")
 const blogsRouter = require("./controllers/blogs")
 const mongoose = require("mongoose")
+const middleware = require("./utils/middleware")
 const config = require("./utils/config")
 const logger = require("./utils/logger")
 
@@ -12,7 +13,11 @@ mongoose.connect(config.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use(middleware.requestLogger)
 
 app.use("/api/blogs", blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
