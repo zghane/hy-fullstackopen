63e77d8fb2ccaace60cf41c974413d5fd5b66ea4
name <>
2022-11-03 17:03:04 +0000

    Part 4.4 - totalLikes function and corresponding tests

---
+++ part4/bloglist/tests/list_helper.test.js
@@ -0,0 +1,110 @@
+const listHelper = require("../utils/list_helper")
+const Blog = require("../models/blog")
+
+test("dummy returns one", () => {
+        const blogs = []
+
+        const result = listHelper.dummy(blogs)
+        expect(result).toBe(1)
+})
+
+// describe groups tests together
+describe("total likes", () => {
+        test("of empty list is zero", () => {
+                const blogs = []
+                const result = listHelper.totalLikes(blogs)
+                expect(result).toBe(0)
+        })
+
+        test("when a list has only one blog equals the likes of that", () => {
+                const blogs = [
+                        new Blog({
+                                title: "Blogtitle",
+                                author: "Mike",
+                                url: "https://mikesblog.wordpress.com",
+                                likes: 100
+                        })
+                ]
+                const result = listHelper.totalLikes(blogs)
+                expect(result).toBe(100)
+        })
+
+        test("of a list of 2 is calculated right", () => {
+                const blogs = [
+                        new Blog({
+                                title: "NaoqponncuNe Mkencasdezo",
+                                author: "Asioirnvkas Ksoiwakc",
+                                url: "https://awneo0ndovcanoewdf.com",
+                                likes: 1000
+                        }),
+                        new Blog({
+                                title: "Qjaonve",
+                                author: "Joasnoec",
+                                url: "https://qjwonaocnoeroiae.com",
+                                likes: 500
+                        }),
+                ]
+
+                const result = listHelper.totalLikes(blogs)
+                expect(result).toBe(1500)
+        })
+
+        // redundant
+        test("of a bigger list is calculated right", () => {
+
+                const blogs = [
+                          new Blog({
+                                      _id: "5a422a851b54a676234d17f7",
+                                      title: "React patterns",
+                                      author: "Michael Chan",
+                                      url: "https://reactpatterns.com/",
+                                      likes: 7,
+                                      __v: 0
+                                    }),
+                          new Blog({
+                                      _id: "5a422aa71b54a676234d17f8",
+                                      title: "Go To Statement Considered Harmful",
+                                      author: "Edsger W. Dijkstra",
+                                      url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+                                      likes: 5,
+                                      __v: 0
+                                    }),
+                          new Blog({
+                                      _id: "5a422b3a1b54a676234d17f9",
+                                      title: "Canonical string reduction",
+                                      author: "Edsger W. Dijkstra",
+                                      url: "http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html",
+                                      likes: 12,
+                                      __v: 0
+                                    }),
+                          new Blog({
+                                      _id: "5a422b891b54a676234d17fa",
+                                      title: "First class tests",
+                                      author: "Robert C. Martin",
+                                      url: "http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll",
+                                      likes: 10,
+                                      __v: 0
+                                    }),
+                          new Blog({
+                                      _id: "5a422ba71b54a676234d17fb",
+                                      title: "TDD harms architecture",
+                                      author: "Robert C. Martin",
+                                      url: "http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html",
+                                      likes: 0,
+                                      __v: 0
+                                    }),
+                          new Blog({
+                                      _id: "5a422bc61b54a676234d17fc",
+                                      title: "Type wars",
+                                      author: "Robert C. Martin",
+                                      url: "http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html",
+                                      likes: 2,
+                                      __v: 0
+                                    })  
+                ]
+                
+                const result = listHelper.totalLikes(blogs)
+                expect(result).toBe(36)
+        })
+
+})
--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
         return 1
 }
 
+// count total number of likes for list of blogs
+const totalLikes = (blogs) => {
+        if (blogs && blogs.length > 0) {
+                return blogs.map(blog => blog.likes).reduce((sum, item) => {
+                        return sum + item
+                })
+        }
+        else {
+                return 0
+        }
+}
+
 module.exports = {
-        dummy
+        dummy,
+        totalLikes
 }
