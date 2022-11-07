a8eb78650033870830d7d5508a511a296c7d8640
name <>
2022-11-07 14:19:35 +0000

    Part 4.17 - add GET /api/users endpoint

--- part4/bloglist/controllers/users.js
+++ part4/bloglist/controllers/users.js
@@ -2,6 +2,10 @@ const bcrypt = require("bcrypt")
 const usersRouter = require("express").Router()
 const User = require("../models/user")
 
+usersRouter.get("/", async (request, response, next) => {
+    const users = await User.find({})
+    response.status(200).json(users)
+})
 usersRouter.post("/", async (request, response, next) => {
     const {username, name, password} = request.body
 
