9a288c5604abdc7a5be196fc45b9c86766688e17
name <>
2022-11-07 15:54:30 +0000

    Part 4.21 - validate login on DELETE /api/blogs/:id

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -9,7 +9,6 @@ blogsRouter.get("/", async (request, response) => {
 })
 
 blogsRouter.post("/", async (request, response) => {
-
     // title and url mandatory
     if (!request.body.title || !request.body.url) {
         response.status(400).end()
@@ -43,34 +42,46 @@ blogsRouter.post("/", async (request, response) => {
     }
 })
 
-blogsRouter.delete("/:id", async (request, response, next) => {
-    try {
-        const deletedBlog = await Blog.findOneAndRemove({id: request.params.id})
-        response.status(204).end()
+blogsRouter.delete("/:id", async (request, response) => {
+    const token = request.token
+    // check if any token is given
+    if (!token) {
+        return response.status(401).json({error: "authorization token missing or invalid"})
     }
-    catch (error) {
-        next(error)
+    // check if given token is valid
+    const decodedToken = jwt.verify(token, process.env.JWT_SIGN_SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({error: "authorization token missing or invalid"})
     }
+
+    // user corresponding to the token
+    const user = await User.findById(decodedToken.id)
+    const blog = await Blog.findById(request.params.id)
+
+    // check that user owns this blog
+    if (blog.user._id.toHexString() !== user.id) {
+        return response.status(401).json({error: "not authorized to delete this blog"})
+    }
+    const deletedBlog = await Blog.findByIdAndRemove(request.params.id)
+    // remove the blog from the user's list of blogs
+    user.blogs = user.blogs.filter(blog => blog.id !== request.params.id)
+    await user.save()
+    response.status(204).end()
 })
 
-blogsRouter.put("/:id", async (request, response, next) => {
-    try {
-        const updatedBlog = {
-            title: request.body.title,
-            author: request.body.author,
-            url: request.body.url,
-            likes: request.body.likes
-        }
-        const blog = await Blog.findOneAndUpdate({id: request.params.id}, updatedBlog, {new: true, runValidators: true, context: "query"})
-        if (blog) {
-            response.status(200).json(blog.toJSON())
-        }
-        else {
-            response.status(400).end()
-        }
+blogsRouter.put("/:id", async (request, response) => {
+    const updatedBlog = {
+        title: request.body.title,
+        author: request.body.author,
+        url: request.body.url,
+        likes: request.body.likes
     }
-    catch (error) {
-        next(error)
+    const blog = await Blog.findOneAndUpdate({id: request.params.id}, updatedBlog, {new: true, runValidators: true, context: "query"})
+    if (blog) {
+        response.status(200).json(blog.toJSON())
+    }
+    else {
+        response.status(400).end()
     }
 })
 
